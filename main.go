c72575b8bd062079a0ecc57ce8e54ecfc2d89111
TeoMatosevic <93163183+TeoMatosevic@users.noreply.github.com>
2024-10-03 14:17:09 +0200

    Add app_offile.htm support

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/fsnotify.v1"
 )
 
 const (
@@ -18,6 +21,29 @@ func main() {
 	var repos Repositories
 	var urls Urls
 	scheduler(&repos, &urls)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if strings.HasSuffix(event.Name, "app_offline.htm") {
+					fmt.Println("Exiting...")
+					os.Exit(0)
+				}
+			}
+		}
+	}()
+
+	currentDir, err := os.Getwd()
+	if err := watcher.Add(currentDir); err != nil {
+		fmt.Println("Error:", err)
+	}
 
 	router := gin.Default()
 
@@ -35,9 +61,9 @@ func main() {
 
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 
-    if port == "" {
-        port = "8080"
-    }
+	if port == "" {
+		port = "8080"
+	}
 
 	router.Run("127.0.0.1:" + port)
 }
